5ad76c71abface126145f3bc02be2499bb22a8d6
김강년 <64246800+KangNyeonKim@users.noreply.github.com>
2023-06-08 21:43:15 +0900

    주문, 할인 도메인 개발

---
+++ src/main/java/hello/core/discount/FixCountPolicy.java
@@ -0,0 +1,16 @@
+package hello.core.discount;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+
+public class FixCountPolicy implements DiscountPolicy{
+    private int discountFixAmount = 1000;
+    @Override
+    public int discount(Member member, int prince) {
+        if (member.getGrade() == Grade.VIP) {
+            return discountFixAmount
+        } else{
+            return 0;
+        }
+    }
+}
