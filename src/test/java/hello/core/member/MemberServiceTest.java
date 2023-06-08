0c5cb72488637fa32e2effbb708c4fde65a33f18
김강년 <64246800+KangNyeonKim@users.noreply.github.com>
2023-06-08 23:18:35 +0900

    새로운 할인 정책 개발

--- src/test/java/hello/core/member/MemberServiceTest.java
+++ src/test/java/hello/core/member/MemberServiceTest.java
@@ -2,8 +2,8 @@ package hello.core.member;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
-@Test
 public class MemberServiceTest {
 
     MemberService memberService = new MemberServiceImpl();
