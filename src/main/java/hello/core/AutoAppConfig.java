3a936d28c6f91d04b86beac452b1e224ee11a433
김강년 <64246800+KangNyeonKim@users.noreply.github.com>
2023-06-14 22:39:53 +0900

    컴포넌트 스캔과 의존관계 자동 주입

---
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -0,0 +1,14 @@
+package hello.core;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+// 기존 작성한 AppConfig에 @Configuration 때문에 AppConfig도 컴포넌트로 등록되게된다.
+// 이들이 등록되면 자동 실행되어 버려서 충돌이 발생한다.
+@ComponentScan(
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
+)
+public class AutoAppConfig {
+}
