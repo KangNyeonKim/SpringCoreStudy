2e32321581a553bc12cdd1f57468a3d8ed7e2004
김강년 <64246800+KangNyeonKim@users.noreply.github.com>
2023-06-16 01:55:30 +0900

    옵션 처리

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -14,7 +14,6 @@ public class OrderServiceImpl implements OrderService {
     //    private final DiscountPolicy discountPolicy = new FixCountPolicy();
     private final DiscountPolicy discountPolicy;
 
-    @Autowired
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
