d2fedf1cf2412c42c99653bd7df395070c80cfd4
김강년 <64246800+KangNyeonKim@users.noreply.github.com>
2023-06-19 05:43:47 +0900

    프로토타입 스코프 - provider

--- src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
+++ src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -2,13 +2,16 @@ package hello.core.scope;
 
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
+import jakarta.inject.Provider;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
 
-import static org.assertj.core.api.Assertions.*;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class SingletonWithPrototypeTest1 {
     @Test
@@ -32,23 +35,23 @@ public class SingletonWithPrototypeTest1 {
         Assertions.assertThat(count1).isEqualTo(1);
         ClientBean clientBean2 = ac.getBean(ClientBean.class);
         int count2 = clientBean2.logic();
-        Assertions.assertThat(count2).isEqualTo(2);
+        Assertions.assertThat(count2).isEqualTo(1);
     }
 
+    @Component
     @Scope("singleton")
     static class ClientBean{
-        private final PrototypeBean prototypeBean;
-
-        public ClientBean(PrototypeBean prototypeBean) {
-            this.prototypeBean = prototypeBean;
-        }
+        @Autowired
+        private Provider<PrototypeBean> prototypeBeansProvider;
 
         public int logic() {
+            PrototypeBean prototypeBean = prototypeBeansProvider.get();
             prototypeBean.addCount();
             return prototypeBean.getCount();
         }
     }
 
+    @Component
     @Scope("prototype")
     static class PrototypeBean {
         private int count = 0;
